af3486b6819edeb3c7eaed420aa99f3f4f735487
tianwenhao <tianwenhao@baidu.com>
2020-03-06 19:06:46 +0800

    b depend on a v0.0.0

---
+++ b.go
@@ -0,0 +1,10 @@
+package b
+
+import (
+	"fmt"
+	"github.com/ticccccc/a"
+)
+
+func Hello()string{
+	return a.Hello()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ticccccc/b
+
+go 1.13
+
+require github.com/ticccccc/a v0.0.0 // indirect
